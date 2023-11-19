a6b515b1d28a88961315e1d9e6ad92125afcf914
Ankit11234 <ankittripathi13486@gmail.com>
2023-11-19 20:23:13 +0530

    uitl.js updated

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -39,7 +39,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${BASE_URL}/signup`,
+        `${BASE_URL}signup`,
         {
           ...inputValue,
         },
